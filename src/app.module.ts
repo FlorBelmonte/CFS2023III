9b22e62c44ed79a6f065c36f45c4fa9a75221633
florbelmonte <mariaflorenciabelmonterojas@gmail.com>
2023-03-25 18:41:10 -0300

    Entrega del ejercicio 3 Clase 20/3 devolver un endpoint con el archivo Pistas.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { PistasController } from './controllers/pistas.controller';
 
 
 @Module({
   imports: [
     // ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'client') }),
-    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'ejercicio2')}),
+    // ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'ejercicio2')}),
+    ServeStaticModule.forRoot({rootPath: join(__dirname, '..', 'ejercicio3')})
   ],
-  controllers: [AppController],
+  controllers: [AppController, PistasController],
   providers: [AppService],
 })
 export class AppModule {}
