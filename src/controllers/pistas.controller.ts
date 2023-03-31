7b7d701cb26eafb20ca6f7053d689a1f08bc2385
florbelmonte <mariaflorenciabelmonterojas@gmail.com>
2023-03-31 18:40:10 -0300

    se modificó el metodo Get

--- src/controllers/pistas.controller.ts
+++ src/controllers/pistas.controller.ts
@@ -3,6 +3,8 @@ import { Controller, Get } from "@nestjs/common";
 import { AppService } from "src/app.service";
 import { Pista } from "src/class/pista";
 import fs from 'fs'
+import * as path from 'path';
+
 
 @Controller("pistas")
 export class PistasController {
@@ -31,8 +33,11 @@ export class PistasController {
   }
 
   @Get() 
-  getJsonMock(): any {
-    const data = fs.readFileSync('./pistas.json', 'utf8');
-    return JSON.parse(data);
+  async getJsonMock(): Promise<any> {
+  const filePath = path.join(__dirname, '../../Client/pistas.json');
+  const contenido = await fs.promises.readFile(filePath, 'utf-8');
+    return {
+      pistas: JSON.parse(contenido),
+    }
   }
 }
