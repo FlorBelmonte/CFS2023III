c7e3b410d3d98c41820020170901425865dde4c6
florbelmonte <mariaflorenciabelmonterojas@gmail.com>
2023-05-12 12:55:43 -0300

    se modifico index.js

--- client/frontConcesionaria/index.js
+++ client/frontConcesionaria/index.js
@@ -1,126 +1,138 @@
-// let contenedor = document.getElementById('contenedor')
+let contenedor = document.getElementById('contenedor');
 
-let btnAgregar = document.getElementById('btnAgregar');
+let btnAgregar = document.getElementById("btnAgregar");
+
 let vehiculos = [];
 
 const mostrarVehiculos = () => {
-  let contenedor = document.getElementById('tblVehiculos');
-  let tabla = '';
-  for (let r of vehiculos) {
-    tabla +=
-      `<tr>
-        <td>${r.marca}</td>
-        <td>${r.patente}</td>
-        <td>${r.modelo}</td>
-        <td>${r.año}</td>
-        <td>${r.precio}</td>
-        <td>${r.capCarga}</td>
-        <td><a href='http://localhost:3000/vehiculoDetail.html?patente=${r.patente}'>Ver detalles</a></td>
-        <td><button type="button" class="btnEliminar" patente="${r.patente}">Eliminar</button></td>
-      </tr>
-      <tr>
-        <td><input type="text" value="${r.marca}" id="marca${r.patente}"></td>
-        <td>${r.patente}</td>
-        <td><input type="text" value="${r.modelo}" id="modelo${r.patente}"></td>
-        <td><input type="text" value="${r.año}" id="año${r.patente}"></td>
-        <td><input type="text" value="${r.precio}" id="precio${r.patente}"></td>
-        <td><input type="text" value="${r.capCarga}" id="capCarga${r.patente}"></td>
-        <td>---</td>
-        <td><button class="btnUpdVehiculo" id="${r.patente}">Actualizar</button></td>
-      </tr>`;
+  let contenedor = document.getElementById("tblVehiculos");
+  let tabla = "";
+  for (let v of vehiculos) {
+    tabla += `<tr>
+      <td>${v.marca}</td>
+      <td>${v.patente}</td>
+      <td>${v.modelo}</td>
+      <td>${v.año}</td>
+      <td>${v.precio}</td>
+      <td>${v.capCarga}</td>
+      <td> <a href='http://localhost:3000/vehiculoDetail.html?patente=${v.patente}' > Ver detalles </a> </td>
+      <td> <button type="button" class="btnEliminar" id="${v.patente}">Eliminar</button></td>
+    </tr>
+    <tr>
+      <td><input type="text" value="${v.marca}" id="marca${v.patente}"</td> 
+      <td>${v.patente}</td>
+
+      <td><input type="text" value="${v.modelo}" id="modelo${v.patente}"</td>
+      <td><input type="number" value="${v.año}" id="año${v.patente}"</td>
+      <td><input type="number" value="${v.precio}" id="precio${v.patente}"</td>
+      <td><input type="number" value="${v.capCarga}" id="capCarga${v.patente}"</td>
+      <td>---</td>
+      <td><button class="btnUpdVehiculo" id="${v.patente}">Actualizar</button></td>
+    </tr>`;
   }
+
   contenedor.innerHTML = tabla;
 
-  const borrarVehiculo = async (e) => {
-    let patente = e.currentTarget.getAttribute('patente');
-    let respuesta = await fetch(`/vehiculos/${patente}`, {
-      method: 'DELETE',
-      headers: { "Content-Type": "application/json" }
-    });
+  // Funcion para borrar vehiculos
+     const borrarVehiculo = async (e) => {
+         let patente = e.target.id;
+          let respuesta = await fetch(`/vehiculos/${patente}`, {
+          method: 'DELETE',
+          headers: { "Content-Type": "application/json" }
+         });
+
     load();
   };
 
   let botonesBorrar = document.querySelectorAll('.btnEliminar');
+
   botonesBorrar.forEach(boton => {
-    boton.addEventListener('click', borrarVehiculo);
+    boton.addEventListener('click', (e) => {
+      borrarVehiculo(e);
+    });
   });
 
-};
+        async function btnActualizarClick(e) {
+         let patente = e.target.id;
 
-async function btnActualizarClick(e) {
-  let patente = e.target.id;
-  let renglon = {
-    "marca": document.querySelector(`#marca${patente}`).value,
-    "modelo": document.querySelector(`#modelo${patente}`).value,
-    "año": Number(document.querySelector(`#año${patente}`).value),
-    "precio": Number(document.querySelector(`#precio${patente}`).value),
-    "capCarga": Number(document.querySelector(`#capCarga${patente}`).value)
-  };
+        let vehiculo = {
+         "marca": document.querySelector(`#marca${patente}`).value,
+         "modelo": document.querySelector(`#modelo${patente}`).value,
+          "año": Number(document.querySelector(`#año${patente}`).value),
+          "precio": Number(document.querySelector(`#precio${patente}`).value),
+          "capCarga": Number(document.querySelector(`#capCarga${patente}`).value)
+         };
 
-  let respuesta = await fetch(`/vehiculos/${patente}`, {
-    method: 'PUT',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(renglon)
-  });
+        let respuesta = await fetch(`/vehiculos/${patente}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(vehiculo)
+    });
 
-  load();
-}
+    load();
+  }
 
-let botonesActualizar = document.querySelectorAll(".btnUpdVehiculo");
-botonesActualizar.forEach(boton => {
-  boton.addEventListener("click", btnActualizarClick);
-});
+  let botonesActualizar = document.querySelectorAll(".btnUpdVehiculo");
 
-async function load() {
-  const url_base = "http://localhost:3000";
-  const endpoint = "/vehiculos";
-  const respuesta = await fetch(url_base + endpoint);
-  vehiculos = await respuesta.json();
-  mostrarVehiculos();
-}
-
-const eliminar = (data) => {
-  console.log("a eliminar", data);
-};
-
-const agregar = async () => {
-  let marca = document.getElementById('marca').value;
-  let patente = document.getElementById('patente').value;
-  let modelo = document.getElementById('modelo').value;
-  let año = document.getElementById('año').value;
-  let precio = document.getElementById('precio').value;
-  let capCarga = document.getElementById('capCarga').value;
-
-  let vehiculo = {
+  botonesActualizar.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+      btnActualizarClick(e);
+    });
+    });
+    
+    };
+    
+    async function load() {
+    const url_base = "http://localhost:3000";
+    const endpoint = "/vehiculos";
+    
+    const respuesta = await fetch(url_base + endpoint);
+    vehiculos = await respuesta.json();
+    console.log(vehiculos);
+    
+    mostrarVehiculos();
+    }
+    
+    const eliminar = (data) => {
+    console.log("a eliminar", data);
+    };
+    
+    const agregar = async () => {
+    let marca = document.getElementById("marca").value;
+    let patente = document.getElementById("patente").value;
+    let modelo = document.getElementById("modelo").value;
+    let año = document.getElementById("año").value;
+    let precio = document.getElementById("precio").value;
+    let capCarga = document.getElementById("capCarga").value;
+    
+    let vehiculo = {
     marca: marca,
     patente: patente,
     modelo: modelo,
     año: Number(año),
     precio: Number(precio),
-    capCarga: Number(capCarga)
-  };
-
-  const response = await postVehiculoServidor(vehiculo);
-
-  if (!Object.keys(response).includes("error")) {
-    vehiculo.patente = response.patente;
+    capCarga: Number(capCarga),
+    };
+    const response = await postVehiculoServidor(vehiculo);
+    
+    if (!Object.keys(response).includes("error")) {
     vehiculos.push(vehiculo);
     mostrarVehiculos();
-  } else {
-    // Manejo de error
-  }
-}
-
-const postVehiculoServidor = async (datos) => {
-  const p = await fetch('/vehiculos', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(datos)
-  });
-  return await p.json();
-}
-
-btnAgregar.addEventListener('click', agregar);
-
-load();
-
+    } else {
+    // manejo de error
+    }
+    };
+    
+    const postVehiculoServidor = async (datos) => {
+    const p = await fetch("/vehiculos", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(datos),
+    });
+    
+    return await p.json();
+    };
+    
+    btnAgregar.addEventListener("click", agregar);
+    
+    load();
